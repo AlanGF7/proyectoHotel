52c8979efcf2ec6464855d77118930659ab5a30a
AlanGF7 <af189107@gmail.com>
2021-10-08 15:24:09 -0600

    Añadida clase java Recámara
    Solo añadimos getter y setter en la clase recamara

---
+++ src/main/java/beans/recamara.java
@@ -0,0 +1,17 @@
+package beans;
+
+
+public class recamara {
+    private int numero;
+    private boolean isOccupied;
+    
+    public int getNumero() { return this.numero; }
+    
+    public void setNumero(int numero) { this.numero = numero; }
+    
+    public boolean getOccupied() { return this.isOccupied; }
+    
+    public void setOccupied(boolean occupied) { this.isOccupied = occupied; }
+    
+    
+}
